83ddfde79d0b07c7174df45c7a91e8e819e152b7
nokkvi96 <nokkvi96@gmail.com>
2021-09-16 22:36:36 +0000

    theme breytt

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,34 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 
 import { ButtonStyle as Button } from "./components";
-import { styles } from "./styles";
 
 const theme = extendTheme({
   colors: {
-    primary: {
-      50: "#eaf8ea",
-      100: "#c9e8c9",
-      200: "#a8d8a8",
-      300: "#85c885",
-      400: "#63b962",
-      500: "#4aa049",
-      600: "#3a7c39",
-      700: "#285928",
-      800: "#173617", // main
-    },
-    background: {
-      50: "#f6f0ef", // main
-      100: "#ded4d2",
-    },
-    secondary: {
-      50: "#fff9da",
-      100: "#ffecad",
-      200: "#ffdf7d",
-      300: "#ffd24b",
-      400: "#ffc51a",
-      500: "#e6ab00", // main
-      600: "#b38500",
-    },
+    primary: "#173617",
+    background: "#f6f0ef",
+    secondary: "#e6ab00",
     red: {
       50: "#ffe4e8",
       100: "#fbb9bf",
