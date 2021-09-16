5844194d6de1486f3071e32576299053baf16e49
nokkvi96 <nokkvi96@gmail.com>
2021-09-16 22:35:28 +0000

    home layout

---
+++ src/modules/home/Section/index.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+import { Box, Container, Stack, Heading, Text } from "@chakra-ui/react";
+
+import img from "@public/plant.svg";
+
+interface IProps {
+  heading: string;
+  paragraph?: string;
+  bgColor?: string;
+  textColor?: string;
+  direction?: "row" | "row-reverse";
+  img?: string;
+}
+
+export function Section({
+  heading,
+  paragraph,
+  bgColor = "secondary",
+  direction = "row",
+  textColor = "white",
+}: // img,
+IProps) {
+  return (
+    <section>
+      <Box py={[6, 8, 12]} bg={bgColor} color={textColor}>
+        <Container maxW="container.xl">
+          <Stack
+            direction={{ base: "column", md: direction }}
+            width="100%"
+            spacing={[6, 8, 12]}
+          >
+            <Box width="100%">
+              <Heading as="h1" size="4xl" mb={[2, 2, 4]}>
+                {heading}
+              </Heading>
+              {paragraph && <Text fontSize="2xl">{paragraph}</Text>}
+            </Box>
+            <Box width="100%">
+              <Image src={img} />
+            </Box>
+          </Stack>
+        </Container>
+      </Box>
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,33 @@
-import { Box, Container, Stack, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
-import { Header, Hero } from "@components";
+import { Header, Footer } from "@components";
+import { Section } from "@modules/home";
 const Home: NextPage = () => {
   return (
     <>
       <Header />
       <main>
-        <Hero mb={[6, 8, 12]} />
-        <Container maxW="container.xl">
-          <Stack direction="column" spacing={[6, 8, 12]}>
-            <p>Heima</p>
-          </Stack>
-        </Container>
+        <Section
+          heading="Kaffi Sel"
+          paragraph="Ut egestas bibendum elementum. Proin tellus nibh, ultrices in scelerisque in, fermentum vitae neque. Proin sed dolor imperdiet, tempor magna sed, fringilla magna. "
+          // img="/@public/plant.svg"
+        />
+        <Section
+          heading="Við erum að tala um kaffi!"
+          paragraph="Quisque pellentesque arcu dictum enim porta aliquam. Vestibulum tincidunt urna semper nisi fringilla, vitae mollis felis pharetra."
+          bgColor="background"
+          textColor="black"
+          direction="row-reverse"
+          // img="/@public/plant.svg"
+        />
+        <Section
+          heading="Við erum að tala um meððí!"
+          paragraph="Pellentesque auctor sem a tellus consectetur consectetur. Donec eu pharetra eros. Suspendisse efficitur, dolor ac iaculis fringilla, arcu purus suscipit ante, nec hendrerit sem felis sit amet nibh. Pellentesque quis risus porttitor, aliquam purus nec, fringilla purus."
+          bgColor="primary"
+
+          // img="/@public/plant.svg"
+        />
+        <Footer />
       </main>
     </>
   );
