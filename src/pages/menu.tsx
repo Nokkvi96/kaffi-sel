215de8b820eed1191deac40d7f1a389c38ca7d1b
nokkvi96 <nokkvi96@gmail.com>
2021-09-28 17:30:49 +0000

    api

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -4,19 +4,32 @@ import type { NextPage } from "next";
 import { Header } from "@components";
 import { ShoppingItems } from "@modules/menu";
 
-const Menu: NextPage = () => {
+const Menu: NextPage = (props) => {
+  console.log(props);
   return (
     <>
       <Header />
       <main>
         <Container maxW="container.xl">
           <Stack direction="column" mt={[6, 8, 12]} spacing={[6, 8, 12]}>
-            <ShoppingItems />
+            <ShoppingItems menu={props.menu} />
           </Stack>
         </Container>
       </main>
     </>
   );
 };
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch("http://localhost:3000/api");
+  const menu = await res.json();
+
+  return {
+    props: {
+      menu,
+    },
+  };
+}
 
 export default Menu;
